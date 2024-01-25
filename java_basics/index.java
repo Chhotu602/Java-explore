69252b809b55c379c10755b20b75def53a6b6a53
Eklavya <141182144+Chhotu602@users.noreply.github.com>
2024-01-25 18:47:59 +0000

    simple

---
+++ java_basics/index.java
@@ -0,0 +1,12 @@
+package java_basics;
+
+import javax.sound.sampled.SourceDataLine;
+
+/**
+ * index
+ */
+public class index {
+    public static void main(String[] args) {
+        System.out.println("Earth");
+    }
+}
