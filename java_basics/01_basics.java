e3c8b071df409116d1402b169df6c4b6b5105e03
Eklavya <141182144+Chhotu602@users.noreply.github.com>
2024-01-31 13:19:50 +0000

    01_basics

---
+++ java_basics/01_basics.java
@@ -0,0 +1,33 @@
+package java_basics;
+
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
+
+/**
+ * index
+ */
+public class index {
+    public static void main(String[] args) {
+    //Variables
+
+    Scanner sc=new Scanner(System.in);
+    String name=sc.next();
+    System.out.println(name);
+    int a=10;
+    int b=5;
+    int sum=a+b;
+    // System.out.println(sum);
+
+    int diff =b-a;
+    // System.out.println(diff);
+
+    int mul=a*b;
+    // System.out.println(mul);
+
+    //To Calculate a*b/a-b
+
+    int ans=(a*b)/(a-b);
+    System.out.println(ans);
+    }
+}
