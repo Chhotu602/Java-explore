28ca54106f3496ccbf5c1504c9b72610f792b305
Eklavya <141182144+Chhotu602@users.noreply.github.com>
2024-02-04 07:25:17 +0000

    half

--- java_basics/pattern.java
+++ java_basics/pattern.java
@@ -2,14 +2,95 @@ package java_basics;
 
 public class pattern {
     public static void main(String[] args) {
-        int n=4;
+        int n=4;                //This is a Global variable.
         int m=5;
         
-        //Outer loop
-        for(int i=1;i<=n;i++){
+        //Outer loop  
+        for(int i=1;i<=n;i++){                         //This is a solid rectangle solution.
             //inner loop
             for(int j=1;j<=m;j++){
-                System.out.print("*");
+                //  System.out.print("*");
+            }
+            // System.out.println();
+        }
+
+        for(int i=1;i<=n;i++){                         //This is a holo rectangle solution.
+            for(int j=1;j<=m;j++){
+                if(j==1 || i==1 || j==m || i==n){
+                    // System.out.print("*");
+                    
+                }
+                else{
+                    // System.out.print(" ");
+                }
+                
+            }
+            // System.out.println();
+        }
+
+
+        for(int i=1;i<=n;i++){                          //This is a Half Pyramid solution.
+            for(int j=1;j<=i;j++){
+                // System.out.print("*");
+            }
+            // System.out.println();
+        }
+
+
+        for(int i=n;i>=1;i--){                          //This is a Inverted Half Pyramid solution.
+            for(int j=i;j>=1;j--){                      //first method
+                // System.out.print("*");
+            }
+            // System.out.println();
+        }
+
+
+        
+        for(int i=n;i>=1;i--){                          //This is also Inverted Half Pyramid solution.
+            for(int j=1;j<=i;j++){                       //second method
+                // System.out.print("*");
+            }
+            // System.out.println();
+        }
+
+
+        for(int i=1;i<=n;i++){                         //This is a Inverted Half Pyramid (rotated by 180 deg ) solution.
+            //inner loop                               //first method
+            for(int sp=n-i;sp>0;sp--){
+                // System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                //  System.out.print("*");
+
+            }
+            // System.out.println();
+        }
+
+
+        for(int i=1;i<=n;i++){                         //This is also Inverted Half Pyramid (rotated by 180 deg ) solution.
+            //inner loop                               //second method
+            for(int sp=1;sp<=n-i;sp++){
+                // System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                //  System.out.print("*");
+
+            }
+            // System.out.println();
+        }
+
+
+        for(int i=1;i<=m;i++){                          //This is a Half Pyramid with Numbers solution.
+            for(int j=1;j<=i;j++){
+                // System.out.print(j);
+            }
+            // System.out.println();
+        }
+        
+
+        for(int i=m;i>=1;i--){                          //This is  Inverted Half Pyramid with Numbers solution.
+            for(int j=1;j<=i;j++){                    
+                System.out.print(j);
             }
             System.out.println();
         }
